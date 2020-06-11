533a52e86c4d8de4ea587d207492017bc1dd6612
franco.fral <franck_ral@hotmail.com>
2020-06-10 21:26:22 -0400

    Initial structure of TODO  project

---
+++ todo-backend/src/main/java/com/umss/todo/TodoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.umss.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoBackendApplication.class, args);
+	}
+
+}
---
+++ todo-backend/src/test/java/com/umss/todo/TodoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umss.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
