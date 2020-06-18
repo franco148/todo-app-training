14578d437a7fda004c065d0280bfdd5eec533dd9
franco.fral <franck_ral@hotmail.com>
2020-06-17 21:32:41 -0400

    backend-frontend-integration: Calling to the REST Services

--- todo-backend/src/main/java/com/umss/todo/controller/UserRestController.java
+++ todo-backend/src/main/java/com/umss/todo/controller/UserRestController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import com.umss.todo.User;
 import com.umss.todo.reposiroty.SqlConnector;
 import com.umss.todo.service.UserService;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/users")
 public class UserRestController {
--- todo-frontend/src/app/app.component.ts
+++ todo-frontend/src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'todo-frontend';
+
+  usersCollection: any = [];
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  ngOnInit(): void {
+    this.httpClient.get('http://localhost:8080/users')
+    .subscribe(response => {
+      this.usersCollection = response;
+      console.log(this.usersCollection);
+    });
+  }
 }
