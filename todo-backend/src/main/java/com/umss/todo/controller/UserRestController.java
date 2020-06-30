5dde267369ec7c7809a6a3794ccf21e4119fc8b0
franco.fral <franck_ral@hotmail.com>
2020-06-29 22:14:11 -0400

    Implementing new features

--- todo-backend/src/main/java/com/umss/todo/controller/UserRestController.java
+++ todo-backend/src/main/java/com/umss/todo/controller/UserRestController.java
@@ -7,12 +7,17 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.umss.todo.controller.dto.TaskRequestDto;
+import com.umss.todo.controller.dto.TaskResponseDto;
 import com.umss.todo.controller.dto.UserCredentialsDto;
+import com.umss.todo.controller.dto.UserRequestDto;
 import com.umss.todo.controller.dto.UserResponseDto;
 import com.umss.todo.service.UserService;
 
@@ -42,4 +47,26 @@ public class UserRestController {
 	public UserResponseDto registerUser(@Valid @RequestBody UserCredentialsDto credentials) {		
 		return userService.registerUser(credentials);
 	}
+	
+	// http://localhost:8080/users/{userId}
+	@PutMapping("/{userId}")
+	public UserResponseDto updateUserProfile(@PathVariable("userId") Long userId,
+											 @Valid @RequestBody UserRequestDto userProfile) {
+		try {
+			return userService.update(userId, userProfile);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	// http://localhost:8080/users/{userId}/task
+	@PostMapping("{userId}/task")
+	public TaskResponseDto addTask(@PathVariable("userId") Long userId,
+								   @Valid @RequestBody TaskRequestDto taskDto) {
+		return null;
+	}
+	
+	
 }
--- todo-backend/src/main/java/com/umss/todo/service/UserService.java
+++ todo-backend/src/main/java/com/umss/todo/service/UserService.java
@@ -1,26 +1,37 @@
 package com.umss.todo.service;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
+import com.umss.todo.controller.dto.TaskRequestDto;
+import com.umss.todo.controller.dto.TaskResponseDto;
 import com.umss.todo.controller.dto.UserCredentialsDto;
+import com.umss.todo.controller.dto.UserRequestDto;
 import com.umss.todo.controller.dto.UserResponseDto;
+import com.umss.todo.reposiroty.TaskRepository;
 import com.umss.todo.reposiroty.UserRepository;
+import com.umss.todo.reposiroty.model.Task;
 import com.umss.todo.reposiroty.model.User;
 
 @Service
 public class UserService {
 
 	private UserRepository userRepository;
+	private TaskRepository taskRepository;
+	private ModelMapper modelMapper;
 	
 	
 	@Autowired
-	public UserService(UserRepository userRepository) {
+	public UserService(UserRepository userRepository,
+					   TaskRepository taskRepository,
+					   ModelMapper modelMapper) {
 		this.userRepository = userRepository;
+		this.taskRepository = taskRepository;
+		this.modelMapper = modelMapper;
 	}
 	
 //	@Autowired
@@ -29,38 +40,49 @@ public class UserService {
 //	}
 	
 	
-	public List<UserResponseDto> getAllUsers() {		
-		List<UserResponseDto> userCollectionResponse = new ArrayList<UserResponseDto>();
+	public List<UserResponseDto> getAllUsers() {
 		
-		List<User> userModelCollection = this.userRepository.findAll();
+		List<UserResponseDto> userModelCollection = this.userRepository
+							 .findAll()
+							 .stream()
+							 .map(user -> {
+								 UserResponseDto response = modelMapper.map(user, UserResponseDto.class);
+                                 return response;												 
+							 }).collect(Collectors.toList());
 		
 		
-		
-		for (User user : userModelCollection) {
-			UserResponseDto userDto = new UserResponseDto();
-			userDto.setId(user.getId());
-			userDto.setFirstName(user.getFirstName());
-			userDto.setLastName(user.getLastName());
-			userDto.setEmail(user.getEmail());
-			
-			userCollectionResponse.add(userDto);
-		}
-		
-		return userCollectionResponse;
+		return userModelCollection;
 	}
 	
 	public UserResponseDto registerUser(UserCredentialsDto credentials) {
-		User userToPersist = new User();
-		userToPersist.setEmail(credentials.getEmail());
-		userToPersist.setPassword(credentials.getPassword());
+		User userToPersist = modelMapper.map(credentials, User.class);
 		
 		User persistedUser = userRepository.save(userToPersist);
-		UserResponseDto userDto = new UserResponseDto();
-		userDto.setId(persistedUser.getId());
-		userDto.setFirstName(persistedUser.getFirstName());
-		userDto.setLastName(persistedUser.getLastName());
-		userDto.setEmail(persistedUser.getEmail());
 		
-		return userDto;
+		return modelMapper.map(persistedUser, UserResponseDto.class);
+	}
+	
+	public UserResponseDto update(Long userId, UserRequestDto userRequestDto) throws Exception {
+		if (!userRepository.existsById(userId)) {
+			throw new Exception("User with ID=" + userId + " does not exist.");
+		}
+		
+		User foundUser = userRepository.getOne(userId);
+		modelMapper.map(userRequestDto, foundUser);
+		
+		User updatedUser = userRepository.save(foundUser);
+		UserResponseDto updatedDto = modelMapper.map(updatedUser, UserResponseDto.class);
+		
+		return updatedDto;
+	}
+	
+	public TaskResponseDto addTask(Long userId, TaskRequestDto taskDto) throws Exception {
+		if (!userRepository.existsById(userId)) {
+			throw new Exception("User with ID=" + userId + " does not exist.");
+		}
+		
+		Task taskToAdd = modelMapper.map(taskDto, Task.class);
+		Task savedTask = taskRepository.save(taskToAdd);
+		return modelMapper.map(savedTask, TaskResponseDto.class);
 	}
 }
