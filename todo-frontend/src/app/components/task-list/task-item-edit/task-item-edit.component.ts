5400856ac5330bd125cfdac6c193516de6bcb3ae
franco.fral <franck_ral@hotmail.com>
2020-07-07 23:00:41 -0400

    todo-app-frontend-implementation: Implemented task-item-edit component.

--- todo-frontend/src/app/components/task-list/task-item-edit/task-item-edit.component.ts
+++ todo-frontend/src/app/components/task-list/task-item-edit/task-item-edit.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+
+import { TaskModel } from 'src/app/data/models/task.model';
+import { DatePipe, formatDate } from '@angular/common';
 
 @Component({
   selector: 'app-task-item-edit',
@@ -7,9 +12,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TaskItemEditComponent implements OnInit {
 
-  constructor() { }
+  taskItem: TaskModel;
+  startTime: string;
+
+  constructor(@Inject(MAT_DIALOG_DATA) public dataParam: any,
+              private dialogRef: MatDialogRef<TaskItemEditComponent>) { }
 
   ngOnInit(): void {
+    this.taskItem = this.dataParam.taskItem;
+    // this.startTime = this.taskItem.startTime.getHours() + ':' + this.taskItem.startTime.getMinutes();
+    // const pipe = new DatePipe("en-US");
+    // this.startTime = formatDate(this.taskItem.startTime, 'h:mm a', 'en-US');
+    console.log('AAAAAAAAAAAAAAAA', this.startTime);
   }
 
+  onSave(form: NgForm) {
+    console.log('Task has been saved...', this.taskItem);
+    // console.log('For start time: ', formatDate(this.startTime, 'MM/DD/YYYY hh:mm A', 'en-US'));
+    // const todayStartTime = new Date();
+    this.dialogRef.close();
+  }
+
+  onCancel() {
+    this.dialogRef.close();
+  }
 }
