571bdbc133cacc297071f1af1804d834cd9ee707
Franco Arratia <franck_ral@hotmail.com>
2020-07-15 23:05:17 -0400

    todo-app-frontend: Implemented authentication guard, and completed the toolbar component

--- todo-frontend/src/app/components/toolbar/toolbar.component.ts
+++ todo-frontend/src/app/components/toolbar/toolbar.component.ts
@@ -1,21 +1,65 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, Output, EventEmitter } from '@angular/core';
+
+import { Subscription } from 'rxjs';
+
+import { UserModel } from 'src/app/data/models/user.model';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-toolbar',
   templateUrl: './toolbar.component.html',
   styleUrls: ['./toolbar.component.css']
 })
-export class ToolbarComponent implements OnInit {
+export class ToolbarComponent implements OnInit, OnDestroy {
 
-  // loggedUser:
+  @Output() sidenavToggle = new EventEmitter<void>();
+  onIsAuthenticationSubscription: Subscription;
+
+  loggedUser: UserModel;
+  isUserAuthenticated = false;
+
   isToggleOpen = false;
 
-  constructor() { }
+
+  constructor(private userService: UserService) { }
 
   ngOnInit(): void {
+    this.onIsAuthenticationSubscription = this.userService.onIsAuthenticationChanged
+    .subscribe(authUser => {
+      this.isUserAuthenticated = authUser != null;
+      this.loggedUser = authUser;
+    });
+
+    this.isUserAuthenticated = this.userService.isUserAuthenticated();
+    this.loggedUser = this.userService.getLoggedUser();
   }
+
 
   onToggleSidenav() {
     this.isToggleOpen = !this.isToggleOpen;
+    this.sidenavToggle.emit();
+  }
+
+  onLogout() {
+    this.userService.logout();
+  }
+
+  getAuthUserInitials() {
+    let profileTextAvatar = this.loggedUser.email;
+
+    if (this.loggedUser.firstName &&
+        this.loggedUser.lastName) {
+
+      profileTextAvatar = this.loggedUser.firstName.charAt(0) +
+                          this.loggedUser.lastName.charAt(0);
+    }
+
+    return profileTextAvatar;
+  }
+
+  ngOnDestroy() {
+    if (this.onIsAuthenticationSubscription) {
+      this.onIsAuthenticationSubscription.unsubscribe();
+    }
   }
 }
--- todo-frontend/src/app/services/task.service.ts
+++ todo-frontend/src/app/services/task.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Subject } from 'rxjs';
+
+import { SERVER_END_POINT_URL } from '../constants/app.constant';
+import { TaskChange } from '../data/interfaces/task-change';
+import { TaskModel } from '../data/models/task.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
 
-  constructor() { }
+  serverUrl = SERVER_END_POINT_URL;
+  onTaskSaveUpdateChanged = new Subject<TaskChange>();
+
+  constructor(private http: HttpClient) { }
+
+
+  addTask(userId: number, task: any) {
+    return this.http.post<TaskModel>(`${this.serverUrl}/users/${userId}/task`, task);
+  }
+
+  getTodaysUserTasks(userId: number) {
+    const todayDate = new Date();
+    const offset = todayDate.getTimezoneOffset()
+    const today = new Date(todayDate.getTime() - (offset*60*1000)).toISOString().split('T')[0];
+
+    return this.http.get<TaskModel[]>(`${this.serverUrl}/users/${userId}/task/filteredby?startDate=${today}&endDate=${today}`);
+  }
+
+  changeTaskState(taskId: number, taskState: string) {
+    return this.http.patch<TaskModel>(`${this.serverUrl}/tasks/${taskId}?state=${taskState}`, {});
+  }
 }
